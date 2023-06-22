d903c63fc8b91abffb8d699984987ad8bb08ed8c
Subhajit <you@example.com>
2023-06-22 21:06:46 +0530

    Updates

---
+++ precache-manifest.18c8b976503049bf68e4737af4d817be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4bb523bb76b991feb401",
+    "url": "/TextUtils/static/css/main.69376326.chunk.css"
+  },
+  {
+    "revision": "4bb523bb76b991feb401",
+    "url": "/TextUtils/static/js/main.4bb523bb.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/TextUtils/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/TextUtils/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "eeac072fcc85ca05faa8",
+    "url": "/TextUtils/static/js/runtime~main.eeac072f.js"
+  },
+  {
+    "revision": "344272449f3e7c8418c5fc632452958d",
+    "url": "/TextUtils/index.html"
+  }
+];
